86676ae291bd42d9528562413b596822ec3bc8a4
marijaspasova00 <119892747+marijaspasova00@users.noreply.github.com>
2022-12-12 18:00:53 +0100

    Update script1.js

--- Class03-Homework#3/Student Exercises/Student Exercise#2/script1.js
+++ Class03-Homework#3/Student Exercises/Student Exercise#2/script1.js
@@ -1,6 +1,6 @@
 function celsiusToFahrenheit(userInputCelsius){
     if(Number.isNaN(userInputCelsius)){
-        console.log("Invalid input!");
+        return "Invalid Input!";
     }
     let result;
     result = ((userInputCelsius*1.8)+32);
@@ -9,15 +9,14 @@ function celsiusToFahrenheit(userInputCelsius){
 
 function fahrenheitToCelsius(userInputFahrenheit){
     if(Number.isNaN(userInputFahrenheit)){
-        console.log("Invalid input!");
+        return "Invalid input!";
     }
     let result;
     result = ((5/9) * (userInputFahrenheit - 32));
     return result;
 }
-
-let userChoise = prompt("C for celsius or F for fahrenheit: ")
 // let userChoiseLower = userChoise.toLowerCase();
+let userChoise = prompt("C for celsius or F for fahrenheit: ")
 let userInput = prompt("Enter the value: ");
 let convertedUserInput = Number(userInput);
 
