433038a57decc92b630db29c79229825f11a39ae
marijaspasova00 <119892747+marijaspasova00@users.noreply.github.com>
2022-12-19 18:52:09 +0100

    Homework

---
+++ Class04-Homework#4/Homework parts/script-minmax.js
@@ -0,0 +1,16 @@
+const numberArray = [13, -15, 1, 0, 55, -3];
+function sumOfMinAndMax(array){
+    let max = -Infinity; //vaka e za posle nekoj br na primer dali -15 e pogolemo od -inf i pogolemo e
+    let min = +Infinity;
+    let sum = 0;
+    for(let num of array){
+        if(num >= max){
+            max = num;
+        } if(num <= min){
+            min = num;
+        }
+    }
+    console.log(`${max}\n${min}`);
+    return (sum = max + min);
+}
+console.log(sumOfMinAndMax(numberArray));
