4e2fca6eb9a0bdac8c547cd38b95cc5f5b2f0734
marijaspasova00 <119892747+marijaspasova00@users.noreply.github.com>
2022-12-21 17:14:34 +0100

    Update script-story.js

--- Class04-Homework#4/Homework parts/script-story.js
+++ Class04-Homework#4/Homework parts/script-story.js
@@ -3,6 +3,7 @@ function tellStory(array){
         if(typeof el !== 'string'){
             return "Invalid input!";
         }
+    }
     return `This is ${array[0]}. ${array[0]} is a nice person.Today they are ${array[1]}. They are ${array[2]} all day. The end.`;
 }
 tellStory("marija", "happy", "playing");
